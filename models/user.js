4c1fbda288e49798b082556a621f2b666b7dad7a
Jackie Wellons <jackie@localhost.localdomain>
2017-05-11 21:19:37 -0700

    backing up progress

--- models/user.js
+++ models/user.js
@@ -1,33 +1,50 @@
+// used to encrypt password in database
+var bcrypt = require("bcrypt-nodejs");
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define("user", {
     // Giving the Author model a name of type STRING
       username: {
 	  type: DataTypes.STRING,
 	  allowNull: false,
+	  validate: {
+	      len: [3]
+	  }
       }, 
       email:  {
 	  type: DataTypes.STRING,
 	  allowNull: false
+	  validate: {
+	      isEmail: true
+	  }
       },
       password: {
 	  type: DataTypes.STRING,
 	  allowNull: false
       }
-  },
-    // Here we'll pass a second "classMethods" object into the define method
-    // This is for any additional configuration we want to give our models
-    {
-      // We're saying that we want our Author to have Posts
+  }, {
+      
+      instanceMethods: {
+	  validPassword: function(password) {
+	      return bcrypt.compareSync(password, this.password);
+	  }
+      },
+      
       classMethods: {
-        associate: function(models) {
-          // Associating Author with Posts
-          // When an Author is deleted, also delete any associated Posts
-          user.hasMany(models.pets, {
-            onDelete: "cascade"
-          });
-        }
+          associate: function(models) {
+              // Associating Author with Posts
+              // When an Author is deleted, also delete any associated Posts
+              user.hasMany(models.pets, {
+		  onDelete: "cascade"
+              });
+          }
+      },
+      hooks: {
+	  beforeCreate: function(user, options, cb) {
+              user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+              cb(null, options);
+	  }
       }
-    }
-  );
-  return user;
+  });			     
+    return user;
 };
