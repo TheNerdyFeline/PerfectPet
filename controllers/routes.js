d56e4ad91aa244aabda3d37c23bdda2863a1d15e
Jackie Wellons <jackie@localhost.localdomain>
2017-05-13 11:05:06 -0700

    updating routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,37 +1,65 @@
-// need to check all routes file names for layout
 var db  = require('../models');
 var express = require('express');
 var router  = express.Router();
 var passport = require("../config/passport");
 
-// this is for current user to log-in
-router.get('/login', function(req, res){
+// homepage load
+router.get("/", function(req , res) {
+    res.render("index");
+});
+
+// current user to log-in page 
+router.get("/login", function(req, res){
     res.render('login');
 });
 
-//this is the new pet form
-router.get('/petreg', function(req,res) {
-	res.render('petreg', {
-    layout: 'pet-registration'
-  });
+// new user sign up page
+router.get("/newuser", function(req, res) {
+    res.render("newuser");
+});
+
+//this is the new pet form page
+router.get("/petreg", function(req,res) {
+	res.render("newpet");
 });
 
 // user signout
-router.get('/sign-out', function(req,res) {
+router.get("/sign-out", function(req,res) {
   req.logout();
   res.redirect("/");
 });
 
+// current pet homepage
+// needs to default to first pet reg how?
+router.get("/:id", function(req, res) {
+    if(req.params.id === null) {
+	db.Pet.findOne({
+	    where: {}
+	});
+    } else {
+	db.Pet.findOne({
+	    where: {id: req.params.id}
+	}).then(function(results) {
+	    res.render("profile");
+	});
+    }
+});
+
+// main feed
+router.get("/mainFeed", function(req, res) {
+    res.render("feed");
+});
+
 
 // login authenticate
-router.post('/login', passport.authenticate("local"), function(req, res) {
+router.post("/login", passport.authenticate("local"), function(req, res) {
     // sending the user back the route to the members page because the redirect will happen on the front end
-  res.json("/");
+  res.json("/:id");
 });
 
 
 // register a new user
-router.post('/signup', function(req,res) {
+router.post("/signup", function(req,res) {
 	db.User.findOne({
     where: {email: req.body.email}
   }).then(function(results) {
@@ -58,33 +86,13 @@ router.post('/signup', function(req,res) {
 router.post("/petreg", function(req, res) {
     db.Pets.create({
 	petName: req.body.name,
-	birthday: req.body.birthday
-	gender: req.body.gender
-	species: req.body.species
+	birthday: req.body.birthday,
+	gender: req.body.gender,
+	species: req.body.species,
 	breed: req.body.breed
     });
 });
 
-// current pet homepage
-// needs to default to first pet reg how?
-router.get("/:id", function(req, res) {
-    if(req.params.id === null) {
-	db.Pet.findOne({
-	    where: {}
-	})
-    } else {
-	db.Pet.findOne({
-	    where: {id: req.params.id}
-	}).then(function(results) {
-	    res.render("");
-	});
-    }
-});
-
-// main feed
-router.get("/mainFeed", function(req, res) {
-    res.render();
-});
 
 // update pet page
 
